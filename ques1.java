c721091f0cbae4ecdcf3866888c12e8bcf66fe4d
Isha <ranaisha2716@gmail.com>
2018-09-13 13:49:25 +0530

    yo

---
+++ ques1.java
@@ -0,0 +1,54 @@
+import java.io.*;
+ class File1
+
+{
+
+	public static void main(String[] x)
+
+	{
+
+		File f = new File("abc.txt");
+
+		
+
+		try{
+
+			FileInputStream fin = new FileInputStream(f);
+
+			
+
+			int i = fin.read();
+
+			
+
+			while(i!=-1)
+
+			{
+
+				System.out.println(i + " : " + (char)i);
+
+				i = fin.read();
+
+			}
+
+				fin.close();
+
+		}
+
+	
+
+		catch(IOException e)
+
+		{
+
+			System.out.println(e);
+
+		}
+
+		
+
+		System.out.print("File path: " + f.getPath());
+
+	}
+
+}
